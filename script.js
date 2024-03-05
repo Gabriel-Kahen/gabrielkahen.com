121f6dba23edc0f41272a3ff5f0ee57e6102503e
Gabriel Kahen <gabekahen@gmail.com>
2024-03-05 18:59:02 -0500

    Body buttons on top right positioned

--- script.js
+++ script.js
@@ -62,12 +62,25 @@ document.querySelectorAll('.bookmark').forEach(element => {
 
 });
 
-/* body */
-
 gabelogo.style.height = screenHeight * .08 + "px";
 gabelogo.style.marginTop = screenHeight * .13 + "px";
 
+/* body */
 
+orangelogo.style.right = screenWidth * .01 + "px";
+orangelogo.style.top = screenHeight * .128 + "px";
+orangelogo.style.width = screenWidth * .03 + "px";
+orangelogo.style.height = screenWidth * .03 + "px";
+
+dotsarray.style.right = screenWidth * .043 + "px";
+dotsarray.style.top = screenHeight * .128 + "px";
+dotsarray.style.width = screenWidth * .03 + "px";
+dotsarray.style.height = screenWidth * .03 + "px";
+
+bodyGmail.style.right = screenWidth * .076 + "px";
+bodyGmail.style.top = screenHeight * .128 + "px";
+bodyGmail.style.width = screenWidth * .03 + "px";
+bodyGmail.style.height = screenWidth * .03 + "px";
 /* functions */
 
 window.addEventListener("resize", resizeURL);
