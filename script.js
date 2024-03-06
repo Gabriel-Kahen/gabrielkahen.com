08b4d082a8d48719befbe178f5cc79910d5e2fe6
Gabriel Kahen <gabekahen@gmail.com>
2024-03-05 19:41:28 -0500

    top of bodybuttons works with screenHeight

--- script.js
+++ script.js
@@ -68,17 +68,17 @@ gabelogo.style.marginTop = screenHeight * .13 + "px";
 /* body */
 
 orangelogo.style.right = imgWidth * .005 + "px";
-orangelogo.style.top = imgWidth * .0465 + "px";
+orangelogo.style.top = screenHeight * .128 + "px";
 orangelogo.style.width = imgWidth * .017 + "px";
 orangelogo.style.height = imgWidth * .017 + "px";
 
-dotsarray.style.right = imgWidth * .026 + "px";
-dotsarray.style.top = imgWidth * .0465 + "px";
+dotsarray.style.right = imgWidth * .025 + "px";
+dotsarray.style.top = screenHeight * .128 + "px";
 dotsarray.style.width = imgWidth * .0165 + "px";
 dotsarray.style.height = imgWidth * .0165 + "px";
 
-bodyGmail.style.right = imgWidth * .047 + "px";
-bodyGmail.style.top = imgWidth * .05 + "px";
+bodyGmail.style.right = imgWidth * .045 + "px";
+bodyGmail.style.top = screenHeight * .138 + "px";
 bodyGmail.style.width = imgWidth * .017 + "px";
 bodyGmail.style.height = screenWidth * .017 + "px";
 /* functions */
