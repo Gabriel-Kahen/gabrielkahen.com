bc748151cd210962aae75ac65d37a44b48c52d98
Gabriel Kahen <gabekahen@gmail.com>
2024-03-09 14:52:58 -0500

    small icons changes and imgWidth fixed

--- script.js
+++ script.js
@@ -3,27 +3,43 @@
 var screenHeight = screen.height;
 var screenWidth = screen.width;
 var imgHeight = (screenHeight * .05)
-var imgWidth = imgHeight * 1942 / 36;
+var imgWidth = imgHeight * 1942 / 96;
+
+function updateTab(){
+  var ratio = window.innerWidth / window.innerHeight;
+  if(ratio < .75){
+      imgWidth = imgHeight * (859 * 80);
+      document.getElementById("tabimg").src = "icons/noiconssmall.png";
+      Email.style.display = 'none';
+      gabrielkahen.style.left = 20/2570 * imgWidth * (8/3) + "px";
+      LinkedIn.style.left = 120/2570 * imgWidth * (8/3) + "px";
+      GitHub.style.left = 491/2570 * imgWidth * (8/3) + "px";
+  }
+  else{
+      imgWidth = imgHeight * 1942 / 36;
+      document.getElementById("tabimg").src = "icons/noicons.png";
+      Email.style.display = 'flex';
+      gabrielkahen.style.left = .0029 * imgWidth * (8/3) + "px";
+      LinkedIn.style.left = .0365 * imgWidth * (8/3) + "px";
+      GitHub.style.left = .0717 * imgWidth * (8/3) + "px";
+      Email.style.left = .106 * imgWidth * (8/3) + "px";
+  }
+}
 
 tabs.style.height = (screenHeight * .042) + "px";
 url.style.height = (screenHeight * .041) + "px";
 bookmarks.style.height = (screenHeight * .035) + "px";
 tabimg.style.height = imgHeight + "px";
 
-gabrielkahen.style.left = 20/2570 * imgWidth + "px";
-LinkedIn.style.left = 250/2570 * imgWidth + "px";
-GitHub.style.left = 491/2570 * imgWidth + "px";
-Email.style.left = 728/2570 * imgWidth + "px";
-
 document.querySelectorAll('.tab').forEach(element => {
     element.style.bottom = (screenHeight * 0.05) / 8 + 'px';
-    element.style.width = imgWidth * .085 + "px";
+    element.style.width = imgWidth * .085 * (8/3) + "px";
 });
 
 /* url */
 
-var size = imgWidth * .0053;
-var spacing = imgWidth * .006;
+var size = imgWidth * .0053 * (8/3);
+var spacing = imgWidth * .006 * (8/3);
 
 document.querySelectorAll('.mod').forEach(element => {
     element.style.width = size + spacing + "px";
@@ -38,20 +54,20 @@ document.querySelectorAll('.modImg').forEach(element => {
 
 account.style.width = (size + spacing) + "px";
 account.style.height = (size + spacing) + "px";
-account.style.bottom =  screenHeight * .0058 + "px";
+account.style.top =  screenHeight * .0048 + "px";
 accountImg.style.width = size * 1.5 + "px";
 accountImg.style.height = size * 1.5 + "px";
 
-arrow_back.style.left = imgWidth * .0037 + "px";
-arrow_forward.style.left = imgWidth * .0161 + "px";
-refresh.style.left = imgWidth * .028 + "px";
+arrow_back.style.left = imgWidth * .0037 * (8/3) + "px";
+arrow_forward.style.left = imgWidth * .0161 * (8/3) + "px";
+refresh.style.left = imgWidth * .028 * (8/3) + "px";
 
-tripledots.style.right = imgWidth * .0034 + "px";
-account.style.right = imgWidth * .0155 + "px";
+tripledots.style.right = imgWidth * .0034 * (8/3) + "px";
+account.style.right = imgWidth * .0155 * (8/3) + "px";
 
 urlbar.style.height = (screenHeight * .041) * .65 + "px";
 urlbar.style.bottom = screenHeight * .004 + "px"
-urlbar.style.left = imgWidth * .042 + "px";
+urlbar.style.left = imgWidth * .042 * (8/3) + "px";
 resizeURL();
 
 /* bookmarks */
@@ -67,26 +83,26 @@ gabelogo.style.marginTop = screenHeight * .2 + "px";
 
 /* body */
 
-orangelogo.style.right = imgWidth * .005 + "px";
+orangelogo.style.right = imgWidth * .005 * (8/3) + "px";
 orangelogo.style.top = screenHeight * .128 + "px";
-orangelogo.style.width = imgWidth * .017 + "px";
-orangelogo.style.height = imgWidth * .017 + "px";
+orangelogo.style.width = imgWidth * .017 * (8/3) + "px";
+orangelogo.style.height = imgWidth * .017 * (8/3) + "px";
 
-dotsarray.style.right = imgWidth * .025 + "px";
+dotsarray.style.right = imgWidth * .025 * (8/3) + "px";
 dotsarray.style.top = screenHeight * .128 + "px";
-dotsarray.style.width = imgWidth * .017 + "px";
-dotsarray.style.height = imgWidth * .017 + "px";
+dotsarray.style.width = imgWidth * .017 * (8/3) + "px";
+dotsarray.style.height = imgWidth * .017 * (8/3) + "px";
 
-bodyGmail.style.right = imgWidth * .045 + "px";
+bodyGmail.style.right = imgWidth * .045 * (8/3) + "px";
 bodyGmail.style.top = screenHeight * .14 + "px";
-bodyGmail.style.width = imgWidth * .017 + "px";
-bodyGmail.style.height = imgWidth * .017 * .5 + "px";
+bodyGmail.style.width = imgWidth * .017 * (8/3) + "px";
+bodyGmail.style.height = imgWidth * .017 * (8/3) * .5 + "px";
 
 /* search */
 
 searchContainer.style.marginTop = screenHeight * .07 + "px";
 searchContainer.style.height = screenHeight * .04 + "px";
-searchIcon.style.left = imgWidth * .0065 + "px";
+searchIcon.style.left = imgWidth * .0065 * (8/3) + "px";
 searchIcon.style.top = screenHeight * .014 + "px";
 
 searchInput.style.height = screenHeight * .025 + "px";
@@ -96,11 +112,12 @@ document.querySelectorAll('.option').forEach(element => {
 
 resizeSearch();
 
-
 /* functions */
 
 window.addEventListener("resize", resizeURL);
 window.addEventListener("resize", resizeSearch);
+window.addEventListener("resize", updateTab);
+
 
 
 urlbar.addEventListener('keypress', function(event) {
@@ -121,9 +138,13 @@ function search(input){
     window.open("https://www.google.com/search?q=" + input);
 }
 
+
+
+updateTab();
+
 function resizeSearch(){
     var ratio = window.innerWidth / window.innerHeight;
-    if(ratio > .8){
+    if(ratio > .75){
         searchContainer.style.width = 550 + "px";
     }
     else{
